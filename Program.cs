3abac7023f5d9a6414027d4b840fa244f1542e65
Lucas Gabriel <lucasga.oliveira68@gmail.com>
2024-03-11 21:23:56 -0300

    projeto-completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("smartphone Nokia:");
+Smartphone nokia = new Nokia("12121212", "modelo-1", "asasasas1", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\nsmartphone Iphone:");
+Smartphone iphone = new Iphone("13131313", "modelo-2", "asasasas2", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
